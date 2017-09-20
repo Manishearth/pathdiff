f64de9f529424c43fe07cd5f16f4160c6fdab224
Manish Goregaokar <manishsmail@gmail.com>
2017-09-19 17:05:45 -0700

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,74 @@
+// Copyright 2012-2015 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+// Adapted from rustc's path_relative_from
+// https://github.com/rust-lang/rust/blob/e1d0de82cc40b666b88d4a6d2c9dcbc81d7ed27f/src/librustc_back/rpath.rs#L116-L158
+
+use std::path::*;
+
+/// Construct a relative path from a provided base directory path to the provided path
+///
+/// ```rust
+/// use pathdiff::diff_paths;
+/// use std::path::*;
+///
+/// let baz: PathBuf = "/foo/bar/baz".into();
+/// let bar: PathBuf = "/foo/bar".into();
+/// let quux: PathBuf = "/foo/bar/quux".into();
+/// assert_eq!(diff_paths(&bar, &baz), Some("../".into()));
+/// assert_eq!(diff_paths(&baz, &bar), Some("baz".into()));
+/// assert_eq!(diff_paths(&quux, &baz), Some("../quux".into()));
+/// assert_eq!(diff_paths(&baz, &quux), Some("../baz".into()));
+/// assert_eq!(diff_paths(&bar, &quux), Some("../".into()));
+///
+/// ```
+pub fn diff_paths(path: &Path, base: &Path) -> Option<PathBuf> {
+    // This routine is adapted from the *old* Path's `path_relative_from`
+    // function, which works differently from the new `relative_from` function.
+    // In particular, this handles the case on unix where both paths are
+    // absolute but with only the root as the common directory.
+    use std::path::Component;
+
+    if path.is_absolute() != base.is_absolute() {
+        if path.is_absolute() {
+            Some(PathBuf::from(path))
+        } else {
+            None
+        }
+    } else {
+        let mut ita = path.components();
+        let mut itb = base.components();
+        let mut comps: Vec<Component> = vec![];
+        loop {
+            match (ita.next(), itb.next()) {
+                (None, None) => break,
+                (Some(a), None) => {
+                    comps.push(a);
+                    comps.extend(ita.by_ref());
+                    break;
+                }
+                (None, _) => comps.push(Component::ParentDir),
+                (Some(a), Some(b)) if comps.is_empty() && a == b => (),
+                (Some(a), Some(b)) if b == Component::CurDir => comps.push(a),
+                (Some(_), Some(b)) if b == Component::ParentDir => return None,
+                (Some(a), Some(_)) => {
+                    comps.push(Component::ParentDir);
+                    for _ in itb {
+                        comps.push(Component::ParentDir);
+                    }
+                    comps.push(a);
+                    comps.extend(ita.by_ref());
+                    break;
+                }
+            }
+        }
+        Some(comps.iter().map(|c| c.as_os_str()).collect())
+    }
+}
